4c333e1a427bc89529ea18215b7ebdf72d0ec8c6
Pj Mara <funpjm@gmail.com>
2016-09-18 10:07:18 -0400

    completed first draft
    so I could save

--- cylinder.groovy
+++ cylinder.groovy
@@ -4,7 +4,7 @@ CSG secondLevel = new Cylinder(10,10,18,(int)30).toCSG()
 CSG movedSecondLevel = secondLevel.movez(23)
 movedSecondLevel.setColor(javafx.scene.paint.Color.GOLD);
 
-CSG thirdLevel = base.movez(45)
+CSG thirdLevel = base.movez(44)
 CSG outside = new Cylinder(9,9,4,(int)30).toCSG()
 CSG inside = new Sphere(7).toCSG()
 
@@ -19,8 +19,13 @@ secondRing.setColor(javafx.scene.paint.Color.BLACK);
 CSG thirdRing = secondRing.movez(24)
 thirdRing.setColor(javafx.scene.paint.Color.BLACK);
 
-CSG fourthCylinder = new Cylinder(10,8,4,(int)30).toCSG()
-CSG movedFourth = fourthCylinder.movez(70)
+CSG fourthCylinder = new Cylinder(10,8,6,(int)30).toCSG()
+CSG movedFourth = fourthCylinder.movez(67)
 
+CSG fifthCylinder = new Cylinder(8,11,12,(int)30).toCSG()
+CSG movedFifth = fifthCylinder.movez(70)
 
-return[base, movedSecondLevel, ring, secondRing, thirdLevel, thirdRing, movedFourth]
+CSG sixthCylinder = new Cylinder(11,2,10,(int)30).toCSG()
+CSG movedSixth = sixthCylinder.movez(82)
+
+return[base, movedSecondLevel, ring, secondRing, thirdLevel, thirdRing, movedFourth, movedFifth, movedSixth]
