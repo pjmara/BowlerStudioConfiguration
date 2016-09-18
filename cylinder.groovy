763da827196bf479c5fabb1594a2d4cb95217f50
Pj Mara <funpjm@gmail.com>
2016-09-18 10:22:37 -0400

    Changed Colors of Jack
    To make it look more Like a headphone jack
    YAY

--- cylinder.groovy
+++ cylinder.groovy
@@ -1,10 +1,12 @@
 CSG base = new Cylinder(10,10,20,(int)30).toCSG()
+base.setColor(javafx.scene.paint.Color.GOLD);
 CSG secondLevel = new Cylinder(10,10,18,(int)30).toCSG()
 
 CSG movedSecondLevel = secondLevel.movez(23)
 movedSecondLevel.setColor(javafx.scene.paint.Color.GOLD);
 
 CSG thirdLevel = base.movez(44)
+thirdLevel.setColor(javafx.scene.paint.Color.GOLD);
 CSG outside = new Cylinder(9,9,4,(int)30).toCSG()
 CSG inside = new Sphere(7).toCSG()
 
@@ -21,12 +23,15 @@ thirdRing.setColor(javafx.scene.paint.Color.BLACK);
 
 CSG fourthCylinder = new Cylinder(10,8,6,(int)30).toCSG()
 CSG movedFourth = fourthCylinder.movez(67)
+movedFourth.setColor(javafx.scene.paint.Color.GOLD);
 
 CSG fifthCylinder = new Cylinder(8,11,12,(int)30).toCSG()
 CSG movedFifth = fifthCylinder.movez(70)
+movedFifth.setColor(javafx.scene.paint.Color.GOLD);
 
 CSG sixthCylinder = new Cylinder(11,2,10,(int)30).toCSG()
 CSG movedSixth = sixthCylinder.movez(82)
+movedSixth.setColor(javafx.scene.paint.Color.GOLD);
 
 CSG masterJack = base.union(movedSecondLevel.union(ring.union(secondRing.union(thirdLevel.union(thirdRing.union(movedFourth.union(movedFifth.union(movedSixth))))))))
 CSG movedMaster = masterJack.movey(40)
