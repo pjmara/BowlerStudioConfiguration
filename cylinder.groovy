df6ae8f6eb50c96cb01d40fdad10aa51223562e8
Pj Mara <funpjm@gmail.com>
2016-09-18 08:21:44 -0400

    made base cylinder
    I needed to get started

--- cylinder.groovy
+++ cylinder.groovy
@@ -1,1 +1,11 @@
-//Your code here
+CSG base = new Cylinder(10,10,20,(int)30).toCSG()
+CSG secondLevel = new Cylinder(10,5,20,(int)30).toCSG()
+
+CSG movedSecondLevel = secondLevel.movez(20)
+
+CSG outside = new Cylinder(9,9,3(int)30).toCSG
+CSG inside = new Sphere(7
+
+CSG ring = cylinder - sphere
+
+return[base, movedSecondLevel]
