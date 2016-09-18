ee083cc375ed9f6009432257df888e955557a06f
Pj Mara <funpjm@gmail.com>
2016-09-18 08:47:39 -0400

    fixes so it can run
    I need to save my code

--- cylinder.groovy
+++ cylinder.groovy
@@ -3,9 +3,12 @@ CSG secondLevel = new Cylinder(10,5,20,(int)30).toCSG()
 
 CSG movedSecondLevel = secondLevel.movez(20)
 
-CSG outside = new Cylinder(9,9,3(int)30).toCSG
-CSG inside = new Sphere(7
+CSG outside = new Cylinder(9,9,3,(int)30).toCSG()
+CSG inside = new Sphere(7).toCSG()
 
-CSG ring = cylinder - sphere
+CSG movedCylinder = outside.movez(10)
+CSG movedSphere = inside.movez(10)
+CSG ring = movedCylinder.difference(movedSphere)
+CSG cylinder2 = base.difference(ring)
 
-return[base, movedSecondLevel]
+return[cylinder2, movedSecondLevel, ring]
