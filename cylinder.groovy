521d12e43b166193739e0834d5807ec99febb0b3
Pj Mara <funpjm@gmail.com>
2016-09-18 10:32:41 -0400

    final minor fixes
    To prepare for class and save for good

--- cylinder.groovy
+++ cylinder.groovy
@@ -1,14 +1,14 @@
-CSG base = new Cylinder(10,10,20,(int)30).toCSG()
+CSG base = new Cylinder(9,9,20,(int)30).toCSG()
 base.setColor(javafx.scene.paint.Color.GOLD);
-CSG secondLevel = new Cylinder(10,10,18,(int)30).toCSG()
+CSG secondLevel = new Cylinder(9,9,18,(int)30).toCSG()
 
 CSG movedSecondLevel = secondLevel.movez(23)
 movedSecondLevel.setColor(javafx.scene.paint.Color.GOLD);
 
 CSG thirdLevel = base.movez(44)
 thirdLevel.setColor(javafx.scene.paint.Color.GOLD);
-CSG outside = new Cylinder(9,9,4,(int)30).toCSG()
-CSG inside = new Sphere(7).toCSG()
+CSG outside = new Cylinder(8,8,4,(int)30).toCSG()
+CSG inside = new Sphere(6).toCSG()
 
 CSG movedCylinder = outside.movez(19)
 CSG movedSphere = inside.movez(19)
@@ -21,15 +21,15 @@ secondRing.setColor(javafx.scene.paint.Color.BLACK);
 CSG thirdRing = secondRing.movez(24)
 thirdRing.setColor(javafx.scene.paint.Color.BLACK);
 
-CSG fourthCylinder = new Cylinder(10,8,6,(int)30).toCSG()
+CSG fourthCylinder = new Cylinder(9,7,6,(int)30).toCSG()
 CSG movedFourth = fourthCylinder.movez(67)
 movedFourth.setColor(javafx.scene.paint.Color.GOLD);
 
-CSG fifthCylinder = new Cylinder(8,11,12,(int)30).toCSG()
+CSG fifthCylinder = new Cylinder(7,10,12,(int)30).toCSG()
 CSG movedFifth = fifthCylinder.movez(70)
 movedFifth.setColor(javafx.scene.paint.Color.GOLD);
 
-CSG sixthCylinder = new Cylinder(11,2,10,(int)30).toCSG()
+CSG sixthCylinder = new Cylinder(10,2,8,(int)30).toCSG()
 CSG movedSixth = sixthCylinder.movez(82)
 movedSixth.setColor(javafx.scene.paint.Color.GOLD);
 
@@ -41,7 +41,10 @@ CSG box2 = box.movez(50)
 CSG movedBox = box2.movex(30)
 
 CSG casing = box2.difference(masterJack)
-CSG movedCasing = casing.movey(90) 
+CSG scaleeCasing = casing.scalex(1.2)
+CSG scalerCasing = scaleeCasing.scaley(1.2)
+CSG scaledCasing = scalerCasing.scalez(1.2)
+CSG movedCasing = scaledCasing.movey(90) 
 
 
-return[movedBox, movedCasing, base, movedSecondLevel, ring, secondRing, thirdLevel, thirdRing, movedFourth, movedFifth, movedSixth, movedMaster]
+return[movedCasing, base, movedSecondLevel, ring, secondRing, thirdLevel, thirdRing, movedFourth, movedFifth, movedSixth,]
