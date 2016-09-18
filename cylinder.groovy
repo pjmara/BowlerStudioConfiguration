8ad8910924b8bb852fb41ec86aa8fb5292a30d07
Pj Mara <funpjm@gmail.com>
2016-09-18 08:59:45 -0400

    first color change, need save
    Wanted to make it look like a real headphone jack

--- cylinder.groovy
+++ cylinder.groovy
@@ -1,14 +1,16 @@
 CSG base = new Cylinder(10,10,20,(int)30).toCSG()
-CSG secondLevel = new Cylinder(10,5,20,(int)30).toCSG()
+CSG secondLevel = new Cylinder(10,10,18,(int)30).toCSG()
 
-CSG movedSecondLevel = secondLevel.movez(20)
+CSG movedSecondLevel = secondLevel.movez(23)
 
-CSG outside = new Cylinder(9,9,3,(int)30).toCSG()
+CSG outside = new Cylinder(9,9,4,(int)30).toCSG()
 CSG inside = new Sphere(7).toCSG()
 
-CSG movedCylinder = outside.movez(10)
-CSG movedSphere = inside.movez(10)
+CSG movedCylinder = outside.movez(19)
+CSG movedSphere = inside.movez(19)
 CSG ring = movedCylinder.difference(movedSphere)
-CSG cylinder2 = base.difference(ring)
+CSG secondRing = ring.movez(21)
+secondRing.setColor(javafx.scene.paint.Color.BLACK);
 
-return[cylinder2, movedSecondLevel, ring]
+
+return[base, movedSecondLevel, ring, secondRing]
